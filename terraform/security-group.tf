6866aa1959af27014d550b261ac4b5d5cdc29557
asmaatarek <asmaatarek06@gmail.com>
2024-10-12 23:26:26 +0300

    update

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -105,7 +105,7 @@ resource "aws_instance" "private_instance_az2" {
 
 
 # Private IPs 
-resource "local_file" "ansible_inventory" {yy
+resource "local_file" "ansible_inventory" {
   filename = "${path.module}../ansible/roles/docker_install/tests/inventory"
   content = <<EOT
   [private_servers]
